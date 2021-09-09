fbb735cc5b1929b1391eda0d3bd89a1b0ba1ff98
gudian1618 <1761844211@qq.com>
2021-09-09 23:14:49 +0800

    new

--- cgb2011-flink-log/src/main/java/com/github/gudian1618/cgb2011flinklog/dataset/TransformationTestDataSet.java
+++ cgb2011-flink-log/src/main/java/com/github/gudian1618/cgb2011flinklog/dataset/TransformationTestDataSet.java
@@ -1,8 +1,10 @@
 package com.github.gudian1618.cgb2011flinklog.dataset;
 
-import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapPartitionFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.util.Collector;
 
 /**
  * @author gudian1618
@@ -17,32 +19,63 @@ public class TransformationTestDataSet {
         // 1.获取执行环境
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         // 2.获取数据源
-        DataSource<String> source = env.readTextFile("3.txt");
+        DataSource<String> source = env.readTextFile("3.txt").setParallelism(3);
+        // DataSource<String> source = env.fromElements("hadoop", "hive", "flume", "kafka", "flink");
         // 3.Transformation 转化
-        source.map(new MapFunction<String, Book>() {
+        source.flatMap(new FlatMapFunction<String, String>() {
                 @Override
-                public Book map(String s) throws Exception {
-                    String[] a = s.split("\\|");
-                    Book book = new Book();
-                    book.setBookName(a[0]);
-                    book.setAuthor(a[1]);
-                    book.setCountry(a[2]);
-                    book.setGender(a[3]);
-                    book.setAge(Integer.parseInt(a[4]));
-                    return book;
+                public void flatMap(String s, Collector<String> collector) throws Exception {
+                    String[] split = s.split("\\|");
+                    for (String a : split) {
+                        collector.collect(a);
+                    }
                 }
             })
+            .mapPartition(new MapPartitionFunction<String, Long>() {
+            @Override
+            public void mapPartition(Iterable<String> iterable, Collector<Long> collector) throws Exception {
+                long l = 0;
+                for (String s : iterable) {
+                    l++;
+                }
+                collector.collect(l);
+            }
+        })
 
-        //     .map(new MapFunction<String, Tuple5<String, String, String, String, Integer>>() {
+        //     .flatMap(new FlatMapFunction<String, String>() {
         //     @Override
-        //     public Tuple5<String, String, String, String, Integer> map(String s) throws Exception {
-        //         String[] a = s.split("\\|");
-        //         return new Tuple5<>(a[0], a[1], a[2], a[3], Integer.parseInt(a[4]));
+        //     public void flatMap(String s, Collector<String> collector) throws Exception {
+        //         String[] split = s.split("\\|");
+        //         for (int i = 0; i < split.length; i++) {
+        //             collector.collect(split[i] + " " + i);
+        //         }
         //     }
         // })
-        // 4.sink输出
-        .print();
-    // 5.提交执行
+
+            // .map(new MapFunction<String, Book>() {
+            //     @Override
+            //     public Book map(String s) throws Exception {
+            //         String[] a = s.split("\\|");
+            //         Book book = new Book();
+            //         book.setBookName(a[0]);
+            //         book.setAuthor(a[1]);
+            //         book.setCountry(a[2]);
+            //         book.setGender(a[3]);
+            //         book.setAge(Integer.parseInt(a[4]));
+            //         return book;
+            //     }
+            // })
+
+            //     .map(new MapFunction<String, Tuple5<String, String, String, String, Integer>>() {
+            //     @Override
+            //     public Tuple5<String, String, String, String, Integer> map(String s) throws Exception {
+            //         String[] a = s.split("\\|");
+            //         return new Tuple5<>(a[0], a[1], a[2], a[3], Integer.parseInt(a[4]));
+            //     }
+            // })
+            // 4.sink输出
+            .print();
+        // 5.提交执行
 
     }
 
