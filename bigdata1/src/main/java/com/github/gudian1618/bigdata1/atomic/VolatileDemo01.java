d5621741f2ba61910b311ab9f3cf4a5f482bae5f
gudian1618 <1761844211@qq.com>
2021-10-31 21:16:00 +0800

    new

---
+++ bigdata1/src/main/java/com/github/gudian1618/bigdata1/atomic/VolatileDemo01.java
@@ -0,0 +1,47 @@
+package com.github.gudian1618.bigdata1.atomic;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/10/31 9:05 下午
+ */
+
+public class VolatileDemo01 {
+
+    public static void main(String[] args) {
+        Date d = new Date();
+        d.i = 10;
+        // 线程A
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("A线程已经启动");
+                while (d.i == 10);
+                System.out.println("A线程已经执行结束");
+            }
+        }).start();
+
+        // 线程B
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // 延迟B线程的启动
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("B线程已经启动成功");
+                    d.i = 20;
+                    System.out.println("B线程执行结束");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+}
+
+class Date {
+    volatile int i;
+}
