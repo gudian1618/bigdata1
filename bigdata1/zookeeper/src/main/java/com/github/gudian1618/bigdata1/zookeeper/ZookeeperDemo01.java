cd52087222aaad6ccbe03f9bc4d03183c5823844
gudian1618 <1761844211@qq.com>
2021-11-09 17:32:52 +0800

    new

--- bigdata1/zookeeper/src/main/java/com/github/gudian1618/bigdata1/zookeeper/ZookeeperDemo01.java
+++ bigdata1/zookeeper/src/main/java/com/github/gudian1618/bigdata1/zookeeper/ZookeeperDemo01.java
@@ -1,8 +1,7 @@
 package com.github.gudian1618.bigdata1.zookeeper;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -17,8 +16,10 @@ import java.util.concurrent.CountDownLatch;
 
 public class ZookeeperDemo01 {
 
+    private ZooKeeper zk;
+
     // 连接zookeeper
-    @Test
+    @Before
     public void connect() throws IOException, InterruptedException {
         // 获取zookeeper对象
         // ConnectString连接zookeeper的ip和端口号
@@ -30,7 +31,7 @@ public class ZookeeperDemo01 {
         // 异步:zookeeper可能还没有建立连接或者还在监控中,测试线程可能已经在抢占资源执行
         CountDownLatch cdl = new CountDownLatch(1);
 
-        ZooKeeper zk = new ZooKeeper("192.168.1.200:2181", 5000, new Watcher() {
+        zk = new ZooKeeper("192.168.1.200:2181", 5000, new Watcher() {
             @Override
             public void process(WatchedEvent watchedEvent) {
                 if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
@@ -42,6 +43,18 @@ public class ZookeeperDemo01 {
         // 即便抢到资源也要阻塞等待
         cdl.await();
         System.out.println("finish");
+    }
+
+    // 创建节点
+    @Test
+    public void createNode() throws InterruptedException, KeeperException {
+        // path 节点路径
+        // data 节点数据
+        // acl 权限策略
+        // createNode 创建节点类型
+        // 返回值 节点名
+        String s = zk.create("/log", "hello zookeeper".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        System.out.println(s);
     }
 
 }
